a8eaa5670567d759588ae483fc1504445149ba61
Gonzalo Sánchez <gonzalo@MacBook-Pro-de-Gonzalo.local>
2026-01-08 11:08:52 -0300

    Config: Hardcode env vars injection in vite.config.js to ensure Vercel compatibility

--- Vantio/Cotizaciones/vite.config.js
+++ Vantio/Cotizaciones/vite.config.js
@@ -1,14 +1,22 @@
 // vite.config.js
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig({
-  plugins: [react()],
-  base: '/',
-  optimizeDeps: {
-    include: ['pdfjs-dist']
-  },
-  worker: {
-    format: 'es'
-  }
+export default defineConfig(({ mode }) => {
+  // eslint-disable-next-line no-undef
+  const env = loadEnv(mode, process.cwd(), '');
+  return {
+    plugins: [react()],
+    base: '/',
+    define: {
+      'import.meta.env.VITE_SUPABASE_URL': JSON.stringify(process.env.VITE_SUPABASE_URL || env.VITE_SUPABASE_URL),
+      'import.meta.env.VITE_SUPABASE_ANON_KEY': JSON.stringify(process.env.VITE_SUPABASE_ANON_KEY || env.VITE_SUPABASE_ANON_KEY),
+    },
+    optimizeDeps: {
+      include: ['pdfjs-dist']
+    },
+    worker: {
+      format: 'es'
+    }
+  };
 });
